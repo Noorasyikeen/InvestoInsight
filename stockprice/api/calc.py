41dcdff49667a85ff61ed3194d2baf25222b0a43
huiye heng <huiye9785@gmail.com>
2023-10-05 02:48:26 +0800

    Deleted calc.py from api folder

--- stockprice/api/calc.py
+++
@@ -1,7 +0,0 @@
-def profit(end_price, start_price):
-    profit = (end_price / start_price) -1
-    return profit
-
-def returns(end_price, start_price):
-    returns = (end_price - start_price) / start_price * 100
-    return returns
