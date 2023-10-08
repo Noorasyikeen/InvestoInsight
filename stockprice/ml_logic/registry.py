3c94dfc9eda15db882598d994f8dde9854a0c959
huiye heng <huiye9785@gmail.com>
2023-10-08 17:50:31 +0800

    Updated registry.py for mlflow

--- stockprice/ml_logic/registry.py
+++ stockprice/ml_logic/registry.py
@@ -10,8 +10,8 @@ from colorama import Fore, Style
 from google.cloud import storage
 
 from stockprice.params import *
-# import mlflow
-# from mlflow.tracking import MlflowClient
+import mlflow
+from mlflow.tracking import MlflowClient
 
 def save_results(params: dict, metrics: dict) -> None:
     """
@@ -20,12 +20,12 @@ def save_results(params: dict, metrics: dict) -> None:
     "{LOCAL_REGISTRY_PATH}/metrics/{current_timestamp}.pickle"
     - (unit 03 only) if MODEL_TARGET='mlflow', also persist them on MLflow
     """
-    # if MODEL_TARGET == "mlflow":
-    #     if params is not None:
-    #         mlflow.log_params(params)
-    #     if metrics is not None:
-    #         mlflow.log_metrics(metrics)
-    #     print("✅ Results saved on MLflow")
+    if MODEL_TARGET == "mlflow":
+        if params is not None:
+            mlflow.log_params(params)
+        if metrics is not None:
+            mlflow.log_metrics(metrics)
+        print("✅ Results saved on MLflow")
 
     timestamp = time.strftime("%Y%m%d-%H%M%S")
 
@@ -79,16 +79,16 @@ def save_model(model) -> None:
 
         return None
 
-    # if MODEL_TARGET == "mlflow":
-    #     mlflow.tensorflow.log_model(
-    #         model=model,
-    #         artifact_path="model",
-    #         registered_model_name=MLFLOW_MODEL_NAME
-    #     )
+    if MODEL_TARGET == "mlflow":
+        mlflow.tensorflow.log_model(
+            model=model,
+            artifact_path="model",
+            registered_model_name=MLFLOW_MODEL_NAME
+        )
 
-    #     print("✅ Model saved to MLflow")
+        print("✅ Model saved to MLflow")
 
-    #     return None
+        return None
 
     return None
 
@@ -161,79 +161,79 @@ def load_model(stage="Production"):
             print(f"\n❌ Error: {str(e)}")
             return None
 
-    # elif MODEL_TARGET == "mlflow":
-    #     print(Fore.BLUE + f"\nLoad [{stage}] model from MLflow..." + Style.RESET_ALL)
+    elif MODEL_TARGET == "mlflow":
+        print(Fore.BLUE + f"\nLoad [{stage}] model from MLflow..." + Style.RESET_ALL)
 
-    #     # Load model from MLflow
-    #     model = None
-    #     mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
-    #     client = MlflowClient()
+        # Load model from MLflow
+        model = None
+        mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+        client = MlflowClient()
 
-    #     try:
-    #         model_versions = client.get_latest_versions(name=MLFLOW_MODEL_NAME, stages=[stage])
-    #         model_uri = model_versions[0].source
+        try:
+            model_versions = client.get_latest_versions(name=MLFLOW_MODEL_NAME, stages=[stage])
+            model_uri = model_versions[0].source
 
-    #         assert model_uri is not None
-    #     except:
-    #         print(f"\n❌ No model found with name {MLFLOW_MODEL_NAME} in stage {stage}")
+            assert model_uri is not None
+        except:
+            print(f"\n❌ No model found with name {MLFLOW_MODEL_NAME} in stage {stage}")
 
-    #         return None
+            return None
 
-    #     model = mlflow.tensorflow.load_model(model_uri=model_uri)
+        model = torch.load(model_uri, map_location=torch.device("cpu"))
 
-    #     print("✅ Model loaded from MLflow")
-    #     return model
-    # else:
-    #     return None
-
-
-
-# def mlflow_transition_model(current_stage: str, new_stage: str) -> None:
-#     """
-#     Transition the latest model from the `current_stage` to the
-#     `new_stage` and archive the existing model in `new_stage`
-#     """
-#     mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
-
-#     client = MlflowClient()
-
-#     version = client.get_latest_versions(name=MLFLOW_MODEL_NAME, stages=[current_stage])
-
-#     if not version:
-#         print(f"\n❌ No model found with name {MLFLOW_MODEL_NAME} in stage {current_stage}")
-#         return None
-
-#     client.transition_model_version_stage(
-#         name=MLFLOW_MODEL_NAME,
-#         version=version[0].version,
-#         stage=new_stage,
-#         archive_existing_versions=True
-#     )
-
-#     print(f"✅ Model {MLFLOW_MODEL_NAME} (version {version[0].version}) transitioned from {current_stage} to {new_stage}")
-
-#     return None
+        print("✅ Model loaded from MLflow")
+        return model
+    else:
+        return None
 
 
-# def mlflow_run(func):
-#     """
-#     Generic function to log params and results to MLflow along with TensorFlow auto-logging
 
-#     Args:
-#         - func (function): Function you want to run within the MLflow run
-#         - params (dict, optional): Params to add to the run in MLflow. Defaults to None.
-#         - context (str, optional): Param describing the context of the run. Defaults to "Train".
-#     """
-#     def wrapper(*args, **kwargs):
-#         mlflow.end_run()
-#         mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
-#         mlflow.set_experiment(experiment_name=MLFLOW_EXPERIMENT)
+def mlflow_transition_model(current_stage: str, new_stage: str) -> None:
+    """
+    Transition the latest model from the `current_stage` to the
+    `new_stage` and archive the existing model in `new_stage`
+    """
+    mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 
-#         with mlflow.start_run():
-#             mlflow.tensorflow.autolog()
-#             results = func(*args, **kwargs)
+    client = MlflowClient()
 
-#         print("✅ mlflow_run auto-log done")
+    version = client.get_latest_versions(name=MLFLOW_MODEL_NAME, stages=[current_stage])
 
-#         return results
-#     return wrapper
+    if not version:
+        print(f"\n❌ No model found with name {MLFLOW_MODEL_NAME} in stage {current_stage}")
+        return None
+
+    client.transition_model_version_stage(
+        name=MLFLOW_MODEL_NAME,
+        version=version[0].version,
+        stage=new_stage,
+        archive_existing_versions=True
+    )
+
+    print(f"✅ Model {MLFLOW_MODEL_NAME} (version {version[0].version}) transitioned from {current_stage} to {new_stage}")
+
+    return None
+
+
+def mlflow_run(func):
+    """
+    Generic function to log params and results to MLflow along with TensorFlow auto-logging
+
+    Args:
+        - func (function): Function you want to run within the MLflow run
+        - params (dict, optional): Params to add to the run in MLflow. Defaults to None.
+        - context (str, optional): Param describing the context of the run. Defaults to "Train".
+    """
+    def wrapper(*args, **kwargs):
+        mlflow.end_run()
+        mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+        mlflow.set_experiment(experiment_name=MLFLOW_EXPERIMENT)
+
+        with mlflow.start_run():
+            mlflow.tensorflow.autolog()
+            results = func(*args, **kwargs)
+
+        print("✅ mlflow_run auto-log done")
+
+        return results
+    return wrapper
