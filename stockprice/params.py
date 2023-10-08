a30bc7d699e36f2492f1d171550b245ae9a1adf0
huiye heng <huiye9785@gmail.com>
2023-10-08 17:50:56 +0800

    Updated params.py for mlflow

--- stockprice/params.py
+++ stockprice/params.py
@@ -11,11 +11,11 @@ BQ_REGION = os.environ.get("BQ_REGION")
 BUCKET_NAME = os.environ.get("BUCKET_NAME")
 GCS_DATASET = os.environ.get("GCS_DATASET")
 INSTANCE = os.environ.get("INSTANCE")
-# MLFLOW_TRACKING_URI = os.environ.get("MLFLOW_TRACKING_URI")
-# MLFLOW_EXPERIMENT = os.environ.get("MLFLOW_EXPERIMENT")
-# MLFLOW_MODEL_NAME = os.environ.get("MLFLOW_MODEL_NAME")
-# PREFECT_FLOW_NAME = os.environ.get("PREFECT_FLOW_NAME")
-# PREFECT_LOG_LEVEL = os.environ.get("PREFECT_LOG_LEVEL")
+MLFLOW_TRACKING_URI = os.environ.get("MLFLOW_TRACKING_URI")
+MLFLOW_EXPERIMENT = os.environ.get("MLFLOW_EXPERIMENT")
+MLFLOW_MODEL_NAME = os.environ.get("MLFLOW_MODEL_NAME")
+PREFECT_FLOW_NAME = os.environ.get("PREFECT_FLOW_NAME")
+PREFECT_LOG_LEVEL = os.environ.get("PREFECT_LOG_LEVEL")
 # EVALUATION_START_DATE = os.environ.get("EVALUATION_START_DATE")
 GCR_IMAGE = os.environ.get("GCR_IMAGE")
 GCR_REGION = os.environ.get("GCR_REGION")
@@ -71,7 +71,7 @@ DTYPES_PROCESSED = {
 ################## VALIDATIONS #################
 
 env_valid_options = dict(
-    MODEL_TARGET=["local", "gcs"],
+    MODEL_TARGET=["local", "gcs", "mlflow"],
 )
 
 def validate_env_value(env, valid_options):
